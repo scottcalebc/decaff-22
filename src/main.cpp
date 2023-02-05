45958c27ac6e376d14faa03cc761a43c044ce7c6
Caleb Scott <calebcscott@outlook.com>
2023-02-05 16:58:38 -0600

    Setup main reading of tokens and printing to screen

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,30 @@
 
 #include "lexer.hpp"
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+#define VERSION 0.1.0
+
+int main(int argc, char** argv) {
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <file_path>" << std::endl;
+        std::cerr << "  file_path    -   path to source file to convert" << std::endl;
+
+        return 1;
+    }
+
+    std::string file_path(argv[1]);
+
+    Lexer lexer = Lexer(file_path);
+    Token token = Token();
+    do {
+        try {
+            token = lexer.getNextToken();
+        } 
+        catch( ... ) {
+            std::cout << "ERROR" << std::endl;
+        }
+    } while (token.type != Token::Type::END);
+
+
+    return 0;
 }
