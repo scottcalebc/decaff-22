63aa9fae24492a9a2a1f50f395ad97dc552f988a
Caleb Scott <calebcscott@outlook.com>
2023-02-06 20:42:22 -0600

    Add Logging Add map lookup for keywords and multi-char operators Add generic character taking loop upon predicate return

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -1,4 +1,5 @@
 #include <regex>
+#include <iostream>
 #include "lexer.hpp"
 #include "predicates.hpp"
 
@@ -19,18 +20,26 @@ void Lexer::nextLine()
  */
 void Lexer::skipWhiteSpace()
 {
-    std::string spaceChars(" \t\n");
+    std::string spaceChars("\t\n");
     char incomingChar;
 
-    lineStream >> incomingChar;
+    lineStream.get(incomingChar);
+    std::cout << "Attempting to take whitespace out..." << std::endl;
+    std::cout << "Starting character: " << incomingChar << std::endl;
     
+    // TODO : This does not eat space characters only tabs and newlines
     while (spaceChars.find(incomingChar) != std::string::npos)
     {
-        lineStream >> incomingChar;
+        lineStream.get(incomingChar);
     }
 
     // whichever character was taken from string that broke loop needs to be put back
-    lineStream.put(incomingChar);
+    lineStream.putback(incomingChar);
+    std::cout << "Putting character back: " << incomingChar << std::endl;
+    std::cout << "Line is now: " << lineStream.str() << std::endl;
+
+    // reduce to loop
+    
 
 }
 
@@ -38,35 +47,62 @@ Token Lexer::getNextToken()
 {
     // if we read new line and it's all white space/empty line then we need to go 
     // ahead and read next line
+    std::cout << "Testing if line is empty" << std::endl;
     while(lineStream.str().empty()) {
+        std::cout << "Empty line getting line which should be :" << std::endl;
         nextLine();
+        std::cout << "line [" << lineNumber << "] : " << lineStream.str() << std::endl;
         skipWhiteSpace();
+        // takeWhile(isWhiteSpace());
 
         // skip comments as well here
     }
 
+    // skip WhiteSpace here for each continued line read
+    skipWhiteSpace();
+
+    std::cout << "Starting token reading..." << std::endl;
+
+    // clear token buffer before progressing
+    tokenBuffer.str(std::string());
+
     // beginning of token generation
     char tmp;
-    lineStream >> tmp;
+    lineStream.get(tmp);
+
+    std::cout << "Starting token character: " << tmp << std::endl;
 
     // find out what type of token it might be with beginning char
     Token token = Token();
+    tokenBuffer << tmp;
+    token.lineNumber = lineNumber;
+    token.colStart = columnNumber;
     // if it starts with a character its likely an identifier
     if ( isAlpha()(tmp) )
     {
+
+        token.type = Token::Type::Identifier;
+        std::cout << "Found Identifier..." << std::endl;
+
         // take while isIdentifier
+        takeWhile(isIdentifier());
+
+        // compare identifier with keywords using map
+        auto op = Token::keywords.find(tokenBuffer.str());
+        if (op != Token::keywords.end())
+        {
+            std::cout << "Token string is keyword found type: " << Token::getTypeName(op->second) << std::endl;
+            token.type = op->second;
+        }
     }
     // if it starts with a number it is either Int or DoubleConst
     else if ( isNumber()(tmp) )
     {
-        
+        token.type = Token::Type::IntConstant;
+        takeWhile(isNumber());
     }
     else if ( isOperator()(tmp) )
     {
-        // load operator into token buffer first
-        tokenBuffer << tmp;
-        token.lineNumber = lineNumber;
-        token.colStart = columnNumber;
         token.type = Token::Type::Operator;
 
         // need to peek to see if next character is also an operator
@@ -75,59 +111,40 @@ Token Lexer::getNextToken()
         {
             std::string cmpString(tokenBuffer.str() + tmp);
 
-            if (cmpString.compare("&&") == 0)
+            // if operator exists in map insert into buffer and adjust type
+            auto op = Token::operators.find(cmpString);
+            if (op != Token::operators.end())
             {
                 tokenBuffer << tmp;
-                token.type = Token::Type::And;
+                token.type = op->second;
             }
-
-            if (cmpString.compare("||") == 0) 
-            {
-                tokenBuffer << tmp;
-                token.type = Token::Type::Or;
-            }
-
-            if (cmpString.compare("<=") == 0) 
-            {
-                tokenBuffer << tmp;
-                token.type = Token::Type::LessEqual;
-            }
-
-            if (cmpString.compare(">=") == 0)
-            {
-                tokenBuffer << tmp;
-                token.type = Token::Type::LessEqual;
-            }
-
-            if (cmpString.compare("==") == 0)
-            {
-                tokenBuffer << tmp;
-                token.type = Token::Type::Equal;
-            }
-            
-            if (cmpString.compare("!=") == 0)
-            {
-                tokenBuffer << tmp;
-                token.type = Token::Type::NotEqual;
-            }
-            
         }
     }
     else if ( isSeparator()(tmp) )
     {
-        tokenBuffer << tmp;
-        token.lineNumber = lineNumber;
-        token.colStart = columnNumber;
         token.type = Token::Type::Separator;
     }
     else if ( tmp == '\"')
     {
+        
+        token.type = Token::Type::StringConstant;
         // take while string constant
+        takeWhile(isNotStringConstantEnd());
+        
+        if (tokenBuffer.str().back() != '\"')
+        {
+            // Throw exception with Token
+            throw(1);
+        }
+        
     }
     else
     {
+        // invalid symbol for token throw generic error
         throw(1);
     }
+
+    token.value = tokenBuffer.str();
 
     return token;
 }
