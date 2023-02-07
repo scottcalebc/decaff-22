933c8cc7fae2ba05ad2fa2b16bc1a86a1fcdacc3
Caleb Scott <calebcscott@outlook.com>
2023-02-06 20:43:29 -0600

    Add predicates that will be used during tokenization

--- src/lexer/predicates.hpp
+++ src/lexer/predicates.hpp
@@ -3,65 +3,91 @@
 class isAlpha
 {
     public:
-        bool operator() (const char &c) const
-        {
-            std::locale locale;
-            
-            return std::isalpha(c, locale);
-        }
+    bool operator() (const char &c) const
+    {
+        std::locale locale;
+        
+        return std::isalpha(c, locale);
+    }
 };
 
 class isIdentifier
 {
     public:
-        bool operator() (const char &c) const
-        {
-            std::locale locale;
-            
-            return std::isalnum(c, locale);
-        }
+    bool operator() (const char &c) const
+    {
+        std::locale locale;
+        
+        return std::isalnum(c, locale);
+    }
 };
 
 class isNumber
 {
     public:
-        bool operator() (const char &c) const
-        {
-            std::locale locale;
-            
-            return std::isdigit(c, locale);
-        }
+    bool operator() (const char &c) const
+    {
+        std::locale locale;
+        
+        return std::isdigit(c, locale);
+    }
 };
 
 class isOperator
 {
     public:
-        bool operator() (const char &c) const
-        {
-            std::string operators("+-*/%<>=|!");
+    bool operator() (const char &c) const
+    {
+        std::string operators("+-*/%<>=|!");
 
-            return operators.find(c) != std::string::npos;
-        }
+        return operators.find(c) != std::string::npos;
+    }
 };
 
 class isSeparator
 {
     public:
-        bool operator() (const char &c) const
-        {
-            std::string operators(";.,{}()");
+    bool operator() (const char &c) const
+    {
+        std::string operators(";.,{}()");
 
-            return operators.find(c) != std::string::npos;
-        }
+        return operators.find(c) != std::string::npos;
+    }
 };
 
-class isNotStringConstantEnd
+class isMatchingCharacters
+{
+    std::string characters;
+
+    public:
+    isMatchingCharacters(std::string characters) :
+        characters(characters)
+    {
+
+    };
+
+    bool operator() (const char &c) const
+    {
+        return characters.find(c) != std::string::npos;
+    };
+};
+
+class isNotStringConstantEnd : public isMatchingCharacters
 {
     public:
-    bool operator() (const char &c) const
-        {
-            std::string operators("\"\n");
-
-            return operators.find(c) != std::string::npos;
-        }
+    isNotStringConstantEnd() :
+        isMatchingCharacters{ "\"\n"}
+    {
+    };
 };
+
+class isWhiteSpace : public isMatchingCharacters
+{
+    public:
+    isWhiteSpace() :
+        isMatchingCharacters{ " \t\n"}
+    {
+    };
+};
+
+
