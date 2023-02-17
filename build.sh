a9361ddd435992f157434a86313d52a8b0148efa
Caleb Scott <calebcscott@outlook.com>
2023-02-16 22:31:31 -0600

    Adjust build logic to check for cmake correctly

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 pushd dependencies &> /dev/null
 
-if [[ ! -z $(command -v cmake) && ! -f ./cmake-3.26.0-rc3-linux-x86_64/bin/cmake ]]; then
+
+if [[ ! -x "$(command -v cmake)" && ! -f ./cmake-3.26.0-rc3-linux-x86_64/bin/cmake ]]; then
     echo "Unpacking dependencies for build"
     tar -xf ./cmake-3.26.0-rc3-linux-x86_64.tar.gz
 fi
