e76c735a7cc9cae955b37fc357ac89a5214a9599
Caleb Scott <calebcscott@outlook.com>
2023-02-16 22:55:21 -0600

    Add backup build if cmake fails on remote

--- build.sh
+++ build.sh
@@ -16,8 +16,27 @@ mkdir -p build
 
 cd build
 
-echo "Configuring project"
-cmake -S .. -B .
 
-echo "Building decaf-22"
-make
+
+echo "Configuring project"
+cmake -S .. -B . 2> /dev/null
+
+if [[ $? -eq 0 ]]; then
+    echo "Building decaf-22"
+    make
+else
+    echo "Error running cmake configure..."
+    echo "Running manual build"
+
+    echo "Cleaning build directory"
+    rm -rf ./build/
+
+    mkdir -p ./build/bin
+
+    for file in $(find ./src -iname "*.cpp" -or "*.hpp"); do
+        cp $file ./build
+    done
+
+    echo "Running g++ build"
+    g++ --std=c++11 build/*.cpp -o build/bin/decaf-22
+fi
