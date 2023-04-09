e13893cc57483b37e2ece32b571c1dea78a60e1c
Caleb Scott <calebcscott@outlook.com>
2023-04-09 15:45:34 -0500

    Add IdEntry for ST

--- src/SymbolTable/generate.hpp
+++ src/SymbolTable/generate.hpp
@@ -7,5 +7,25 @@
 
 namespace SymbolTable {
 
+    class IdEntry {
+
+        public:
+            std::string ident;
+            Scanner::Token::Type type;
+
+            /*
+                1 == global
+                2 == parameters
+                3 == local
+            */
+            int block;
+
+            /*
+                offset from base pointer used for code gen
+            */
+            int offset;
+
+    };
+
     void generate(AST::Program *prog);
 }
