5f6955bd309bee42c293b7be2a9ea8d5d688d704
Caleb Scott <calebcscott@outlook.com>
2023-04-25 17:23:43 -0500

    Start code gen visitor

---
+++ src/code-gen/CodeGenVisitor.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <iostream>
+
+#include <visitor/astVisitor.hpp>
+#include <AST/AbstractSyntaxTree.hpp>
+
+
+namespace CodeGen {
+
+    class CodeGenVisitor: public Visitor {
+
+
+            // Overloads of Visitor Interface
+            // default acceptor to show missing virtual functions
+            void visit(Acceptor *a) { std::cout << "STTypeVisitor: Got acceptor" << std::endl; };
+
+            // Ident will lookup in ST and return type
+            void visit(AST::Ident *p){};
+            // Constant will simply set their type
+            void visit(AST::Constant *p){};
+
+            // Expressions
+
+            // arithmetic expressions
+            void visit(AST::Add *p){};
+            void visit(AST::Assign *p){};
+            void visit(AST::Divide *p){};
+            void visit(AST::Modulus *p){};
+            void visit(AST::Subtract *p){};
+            void visit(AST::Multiply *p){};
+
+            // logical  epxressions
+            void visit(AST::Or *p){};
+            void visit(AST::And *p){};
+            void visit(AST::Not *p){};
+            void visit(AST::GTE *p){};
+            void visit(AST::LTE *p){};
+            void visit(AST::Equal *p){};
+            void visit(AST::NotEqual *p){};
+            void visit(AST::LessThan *p){};
+            void visit(AST::GreaterThan *p){};
+
+            // function calls
+            void visit(AST::Call *p){};
+            void visit(AST::Print *p){};
+            void visit(AST::ReadLine *p){};
+            void visit(AST::ReadInteger *p){};
+            
+            // keyword statments, break has no outtype
+            void visit(AST::If *p){};
+            void visit(AST::For *p){};
+            void visit(AST::Break *p){};
+            void visit(AST::While *p){};
+            void visit(AST::Return *p){};
+            void visit(AST::KeywordStmt *p) {};
+
+
+            void visit(AST::StatementBlock *p){};
+            void visit(AST::FunctionDeclaration *p){};
+            void visit(AST::Program *p){};
+
+    }
+}
